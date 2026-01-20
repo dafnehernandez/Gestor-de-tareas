dd177e1cef3f47a96c6a1e07804472893c7fb5ff
Dafne Hernandez <mcflurrycerezita@gmail.com>
2026-01-20 03:50:50 -0600

    feat: adición de la función update y delete de acuerdo al id de la tarea

--- src/store/tasks-slice.js
+++ src/store/tasks-slice.js
@@ -10,15 +10,38 @@ const tasksSlice = createSlice({  //se crea un módulo llamado tasks, porque sli
   //tipo CRUD
   reducers: { //cosas que el usuario puede hacer con las tareas // para cargar tareas desde una API o leer desde localStorage
     setTasks: (state, action) => {
-      state.list = action.payload
+      state.list = action.payload;
     }, //aqui se reemplazan todas las tareas por las nuevas
     
     //agregar nueva tarea al inicio de la lista
     addTask: (state, action) => {
-      state.list.unshift(action.payload) //unshift: inserta al princpio recordar
+      state.list.unshift(action.payload); //unshift: inserta al princpio recordar
     },
+
+    //actualizar tarea, se emplea al editar tarea y al marcar una tarea como completada
+    updateTask: (state, action) => {
+        const index = state.list.findIndex(
+            task => task.id === action.payload.id
+        ) //guarda el index
+          //findIndex es una función que recorre array uno por uno aplica condición y retorna la posición del primer elemento que cumpla la condición o -1
+          //Se busca en lista de tareas el index de la tarea cuyo id sea igual al id enviado
+
+        // function(task) {
+        //     return task.id === action.payload.id
+        // } reemplazado con la funcion flecha de arriba
+      
+        if (index !== -1) { //si existe la tarea entonces la reemplaza con la versión actualizada
+            state.list[index] = action.payload;
+        }
+    },
+
+    //borrar tareas
+    deleteTask: (state, action) => {
+      state.list = state.list.filter(task => task.id !== action.payload) //se borra la tarea cuyo id coincida
+      // filter crea una nueva lista sin esa tarea
+    }
   }
 })
 
 export const { setTasks, addTask, updateTask, deleteTask } = tasksSlice.actions
-export default tasksSlice.reducer
+export default tasksSlice.reducer //también se exporta a reducer
